50c571819d2c7318d3105b0f2f90e2dd4be65809
madhushreehuligudda-prog <madhushreehuligudda@gmail.com>
2025-10-06 16:57:21 +0530

    save

---
+++ account.py
@@ -0,0 +1,4 @@
+initial_balance = float(input("Enter your initial balance: ₹"))
+deposit_amount = float(input("Enter the deposit amount: ₹"))
+updated_balance = initial_balance + deposit_amount
+print(f"Updated balance after deposit: ₹{updated_balance:.2f}")
