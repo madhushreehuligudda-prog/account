104bf25eee0fcc72d8c0313eed5d7b2a4c317fb4
madhushreehuligudda-prog <madhushreehuligudda@gmail.com>
2025-10-06 17:05:11 +0530

    save to withdraw-feature

--- account.py
+++ account.py
@@ -1,4 +1,10 @@
 initial_balance = float(input("Enter your initial balance: ₹"))
 deposit_amount = float(input("Enter the deposit amount: ₹"))
-updated_balance = initial_balance + deposit_amount
-print(f"Updated balance after deposit: ₹{updated_balance:.2f}")
+balance = initial_balance + deposit_amount
+print(f"Updated balance after deposit: ₹{balance:.2f}")
+withdraw_amount = float(input("Enter the withdrawal amount: ₹"))
+if withdraw_amount > balance:
+    print("Insufficient funds! Withdrawal denied.")
+else:
+    balance -= withdraw_amount
+    print(f"Updated balance after withdrawal: ₹{balance:.2f}")
